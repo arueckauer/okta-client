f97bc1f62583442ded713649a991be5656bd2c9f
Andi Rückauer <arueckauer@gmail.com>
2021-10-14 10:23:54 +0200

    Add http header options

---
+++ src/Http/HeaderOptions.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OktaClient\Http;
+
+class HeaderOptions
+{
+    public const ACCEPT        = 'Accept';
+    public const AUTHORIZATION = 'Authorization';
+    public const CONTENT_TYPE  = 'Content-Type';
+}
