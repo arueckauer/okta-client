d7d7faf0dfb4b2582f00ada6b5369c8570f62236
Andi Rückauer <arueckauer@gmail.com>
2022-09-02 14:35:12 +0200

    Replaces usage of deprecated Interop\Container\ContainerInterface with Psr\Container\ContainerInterface

--- src/Request/GenericFactory.php
+++ src/Request/GenericFactory.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace OktaClient\Request;
 
-use Interop\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use OktaClient\Client;
+use Psr\Container\ContainerInterface;
 
 use function assert;
 
